988d4895d6c3c42495639e7d56a9bebe3c89c841
shloke1106@gmail.com <shloke1106@gmail.com>
2025-01-24 01:57:14 +0530

    'changes'

--- src/components/CardScreen.jsx
+++ src/components/CardScreen.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { motion } from "framer-motion";
+import { easeInOut, motion } from "framer-motion";
 import backgroundImage from "../assets/bg.png";
 import logo from "../assets/logo.svg";
 import image from "../assets/image.svg";
@@ -9,8 +9,8 @@ const CardScreen = () => {
   const navigate = useNavigate();
 
   const cardScreenVariants = {
-    hidden: { opacity: 0 },
-    visible: { opacity: 1, transition: { duration: 1 } },
+    hidden: { opacity: 0, ease: "easeInOut" },
+    visible: { opacity: 1, transition: { duration: 1.5 } },
   };
 
   const handleHome = () => {
@@ -20,120 +20,118 @@ const CardScreen = () => {
   return (
     <>
       <div
-        
         style={{
           backgroundImage: `url(${backgroundImage})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
       >
-          <div className="logo-container py-4 text-center mb-4 cursor-pointer"
-                  style={{
-                    backgroundImage: `url(${backgroundImage})`,
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                  }}>
-            <img
-              src={logo}
-              alt="The Wedding Company"
-              className="h-12 mx-auto"
-              onClick={handleHome}
-            />
-            <p className="text-yellow-700 font-medium opacity-70">
-              THE WEDDING COMPANY
-            </p>
-          </div>
-        <div className="card-screen-container flex flex-col lg:flex-row bg-gradient-to-r from-white to-[#FDFDF1] h-screen w-screen"
-                style={{
-                  backgroundImage: `url(${backgroundImage})`,
-                  backgroundSize: "cover",
-                  backgroundPosition: "center",
-                }}>
-        <motion.div
-          className="card-screen-content flex flex-col lg:flex-row w-full h-full"
-          variants={cardScreenVariants}
-          initial="hidden"
-          animate="visible"
+        <div
+          className="logo-container py-4 text-center mb-4 cursor-pointer"
+          style={{
+            backgroundImage: `url(${backgroundImage})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
         >
-          {/* Left Section */}
-          <div className="flex justify-center items-center w-lg lg:w-1/2 p-4">
-            <img
-              src={image}
-              alt="Event Planning"
-              className="w-full max-w-lg lg:max-w-lg"
-            />
-          </div>
-
-          {/* Right Section */}
-          <div className="flex flex-col justify-center items-center w-full lg:w-1/2 px-6 lg:px-12">
-            {/* Heading */}
-            <h1 className="text-2xl lg:text-4xl font-medium leading-tight text-gray-700 mb-6 ">
-              Anita, let our expert planners <br /> craft your special day
-            </h1>
-
-            {/* Features */}
-            <div className="bg-white rounded-3xl shadow-lg p-6 mb-6 w-full max-w-lg">
-              <div className="flex items-center mb-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-6 text-yellow-500"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 3v2.25m0 4.15c0 .328-.11 
-.636-.29.9-1.04.235-2.098.387-3.129.47M8.188 9.188a4.5 4.5 0 01-1.977.176A4.5 4.5 0 015.25 9.5c0 .42.17 
-.812.45.118S6.5 9.94 7 10c.5.06.965.128 1.4.188M20.812 9.188a4.5 4.5 0 01-1.977.176A4.5 4.5 0 0118.75 9.5c0 .42.17.812.45.118S20 9.94 20.5 10c.5.06.965.128 1.4.188m-4.5 10h.01a9 9 0 013.138-4.162 9 9 0 01-3.138 4.162zm0-7.5h.01a2.25 2.25 0 001.586.44 2.25 2.25 0 00-1.586-.44z"
-                  />
-                </svg>
-                <h2 className="text-lg lg:text-xl font-semibold text-gray-700 ml-2">
-                  Unlock best venues, decors & more
-                </h2>
-              </div>
-              <p className="text-gray-400 font-medium text-xl">
-                Tell us about your dream day & get a perfect proposal in your
-                budget for FREE.
-              </p>
+          <img
+            src={logo}
+            alt="The Wedding Company"
+            className="h-12 mx-auto"
+            onClick={handleHome}
+          />
+          <p className="text-yellow-700 font-medium opacity-70">
+            THE WEDDING COMPANY
+          </p>
+        </div>
+        <div
+          className="card-screen-container flex flex-col lg:flex-row bg-gradient-to-r from-white to-[#FDFDF1] h-screen w-screen"
+          style={{
+            backgroundImage: `url(${backgroundImage})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        >
+          <motion.div
+            className="card-screen-content flex flex-col lg:flex-row w-full h-full"
+            variants={cardScreenVariants}
+            initial="hidden"
+            animate="visible"
+          >
+            <div className="flex justify-center items-center w-full p-4">
+              <img src={image} alt="Event Planning" className="w-full max-w-lg" />
             </div>
 
-            <div className="bg-white rounded-3xl shadow-lg p-6 w-full max-w-lg">
-              <div className="flex items-center mb-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-6 text-yellow-500"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M12 3v2.25m0 4.15c0 .328-.11 
+            <div className="flex flex-col justify-center items-center w-full lg:w-1/2 px-6 lg:px-12">
+              <h1 className="text-2xl lg:text-4xl font-medium leading-tight text-gray-700 mb-6 ">
+                Anita, let our expert planners <br /> craft your special day
+              </h1>
+
+              <div className="bg-white rounded-3xl shadow-lg p-6 mb-6 w-full max-w-lg">
+                <div className="flex items-center mb-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6 text-yellow-500"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 3v2.25m0 4.15c0 .328-.11 
 .636-.29.9-1.04.235-2.098.387-3.129.47M8.188 9.188a4.5 4.5 0 01-1.977.176A4.5 4.5 0 015.25 9.5c0 .42.17 
 .812.45.118S6.5 9.94 7 10c.5.06.965.128 1.4.188M20.812 9.188a4.5 4.5 0 01-1.977.176A4.5 4.5 0 0118.75 9.5c0 .42.17.812.45.118S20 9.94 20.5 10c.5.06.965.128 1.4.188m-4.5 10h.01a9 9 0 013.138-4.162 9 9 0 01-3.138 4.162zm0-7.5h.01a2.25 2.25 0 001.586.44 2.25 2.25 0 00-1.586-.44z"
-                  />
-                </svg>
-                <h2 className="text-lg lg:text-xl font-semibold text-gray-700 ml-2">
-                  800+ Flawless Celebrations
-                </h2>
+                    />
+                  </svg>
+                  <h2 className="text-lg lg:text-xl font-semibold text-gray-700 ml-2">
+                    Unlock best venues, decors & more
+                  </h2>
+                </div>
+                <p className="text-gray-400 font-medium text-xl">
+                  Tell us about your dream day & get a perfect proposal in your
+                  budget for FREE.
+                </p>
               </div>
-              <p className="text-gray-400 font-medium text-xl ">
-                Enjoy a perfect, stress-free wedding from the first visit to the
-                final goodbyes.
-              </p>
-            </div>
 
-            {/* Button */}
-            <button className="bg-pink-500 w-lg text-white text-lg lg:text-xl px-12 py-6 cursor-pointer rounded-full shadow-md hover:shadow-lg ring shadow-yellow-500/50 mt-8">
-              Get My FREE Proposal
-            </button>
-          </div>
-        </motion.div>
+              <div className="bg-white rounded-3xl shadow-lg p-6 w-full max-w-lg">
+                <div className="flex items-center mb-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6 text-yellow-500"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M12 3v2.25m0 4.15c0 .328-.11 
+.636-.29.9-1.04.235-2.098.387-3.129.47M8.188 9.188a4.5 4.5 0 01-1.977.176A4.5 4.5 0 015.25 9.5c0 .42.17 
+.812.45.118S6.5 9.94 7 10c.5.06.965.128 1.4.188M20.812 9.188a4.5 4.5 0 01-1.977.176A4.5 4.5 0 0118.75 9.5c0 .42.17.812.45.118S20 9.94 20.5 10c.5.06.965.128 1.4.188m-4.5 10h.01a9 9 0 013.138-4.162 9 9 0 01-3.138 4.162zm0-7.5h.01a2.25 2.25 0 001.586.44 2.25 2.25 0 00-1.586-.44z"
+                    />
+                  </svg>
+                  <h2 className="text-lg lg:text-xl font-semibold text-gray-700 ml-2">
+                    800+ Flawless Celebrations
+                  </h2>
+                </div>
+                <p className="text-gray-400 font-medium text-xl ">
+                  Enjoy a perfect, stress-free wedding from the first visit to
+                  the final goodbyes.
+                </p>
+              </div>
+
+              {/* <button className="bg-pink-500 w-lg text-white text-lg lg:text-xl px-12 py-6 cursor-pointer rounded-full shadow-md hover:shadow-lg ring shadow-yellow-500/50 mt-8">
+                Get My FREE Proposal
+              </button> */}
+
+              <button className="bg-pink-500 text-white text-lg px-6 py-4 cursor-pointer rounded-full shadow-md hover:shadow-lg ring shadow-yellow-500/50 mt-8 w-full lg:w-lg ">
+                Get My FREE Proposal
+              </button>
+            </div>
+          </motion.div>
         </div>
       </div>
     </>
--- src/components/IntroScreen.jsx
+++ src/components/IntroScreen.jsx
@@ -1,60 +1,113 @@
-import React from "react";
-import { motion } from "framer-motion";
+import React, { useState }  from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 import backgroundImage from "../assets/bg.png";
 import envelope from "../assets/letter.svg";
 import card from "../assets/card.svg";
+import logo from "../assets/logo.svg";
 
 const IntroScreen = () => {
   const navigate = useNavigate();
-
+  const [isVisible, setIsVisible] = useState(true);
   // Animation Variants
   const envelopeVariants = {
     hidden: { y: "100vh", opacity: 0 },
-    visible: { y: "0vh", opacity: 1, transition: { type: "spring", duration: 2 } },
-    exit: { y: "-50vh", opacity: 0, transition: { duration: 1 } },
+    visible: {
+      y: "0vh",
+      opacity: 1,
+      transition: { type: "spring", duration: 3 },
+    },
+    exit: {
+      y: "-100vh",
+      opacity: 0,
+      transition: { duration: 1.5, ease: "easeInOut" },
+    },
   };
 
   const cardVariants = {
-    hidden: { y: "100%", scale: 1.5, opacity: 0 },
-    visible: { y: "0%", scale: 1, opacity: 1, transition: { type: "spring", duration: 2, delay: 1 } },
+    hidden: { y: "100%", scale: 0.8, opacity: 0 },
+    visible: {
+      y: "50%",
+      scale: 1.5,
+      opacity: 1,
+      transition: { type: "spring", duration: 3, delay: 1 }, // Delayed to match envelope
+    },
+    exit: {
+      y: "-100vh",
+      opacity: 0,
+      transition: { duration: 1.5, ease: "easeInOut" },
+    },
   };
 
   const handleNext = () => {
-    setTimeout(() => navigate("/card"), 1500); // Delay navigation for exit animation
+    setIsVisible(false);
+    setTimeout(() => navigate("/card"), 600); // Ensure navigation happens after the exit animation
   };
 
   return (
-    <motion.div
-      className="intro-container flex justify-center items-center h-screen w-screen"
-      style={{
-        backgroundImage: `url(${backgroundImage})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-      initial={{ opacity: 1 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0, transition: { duration: 1 } }}
-    >
-      <motion.div
-        className="envelope-container relative flex justify-center items-center"
-        variants={envelopeVariants}
-        initial="hidden"
-        animate="visible"
-        exit="exit"
-        onAnimationComplete={handleNext}
-      >
-        <img src={envelope} alt="Envelope" className="w-64 lg:w-80" />
-        <motion.div
-          className="greeting-card absolute top-0 left-1/2 transform -translate-x-1/2"
-          variants={cardVariants}
-          initial="hidden"
-          animate="visible"
+    <>
+              <div
+              className="logo-container py-4 text-center cursor-pointer"
+              style={{
+                backgroundImage: `url(${backgroundImage})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            >
+              <img
+                src={logo}
+                alt="The Wedding Company"
+                className="h-12 mx-auto"
+              />
+              <p className="text-yellow-700 font-medium opacity-70">
+                THE WEDDING COMPANY
+              </p>
+            </div>
+    <AnimatePresence>
+      {
+        isVisible && (
+          <motion.div
+          className="intro-container flex justify-center items-center h-screen w-screen"
+          style={{
+            backgroundImage: `url(${backgroundImage})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+          // initial={{ opacity: 0 }}
+          
+          
+          // animate={{ opacity: 1 }}
+          
+          // exit={{ opacity: 0, transition: { duration: 1 } }}
+          
         >
-          <img src={card} alt="Greeting Card" className="w-48 lg:w-64" />
+          <motion.div
+            className="envelope-container relative flex justify-center items-center"
+            variants={envelopeVariants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            onAnimationComplete={handleNext}
+          >
+            {/* Envelope */}
+            <img src={envelope} alt="Envelope" className="w-64 lg:w-80" />
+            {/* Card */}
+            <motion.div
+              className="greeting-card absolute top-0 left-1/2 transform -translate-x-1/2"
+              variants={cardVariants}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+            >
+              <img src={card} alt="Greeting Card" className="w-48 lg:w-64" />
+            </motion.div>
+          </motion.div>
         </motion.div>
-      </motion.div>
-    </motion.div>
+        )
+      }
+    </AnimatePresence>
+    </>
+
   );
 };
 
